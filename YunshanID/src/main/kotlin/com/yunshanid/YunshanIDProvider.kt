56cff2b0a0138ef88d3cd50658283a53a4013116
mozzaru <100791931+mozzaru@users.noreply.github.com>
2026-05-16 11:04:08 +0000

    feat: add YunshanID extension and latest releases
    - Implemented YunshanID class using its JSON API (api/donghuas, api/donghua/{id})
    - Added support for latest, popular, and search
    - Added YunshanIDProvider for registration
    - Set up build configuration for YunshanID
    - Reverted unintentional global build.gradle.kts changes

---
+++ YunshanID/src/main/kotlin/com/yunshanid/YunshanIDProvider.kt
@@ -0,0 +1,11 @@
+package com.yunshanid
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class YunshanIDProvider: Plugin() {
+    override fun load() {
+        registerMainAPI(YunshanID())
+    }
+}
